8a1f74f6cb7077295f9d18fb585de11aadeeb39c
Eric Minassian <eric@ericminassian.com>
2023-08-13 09:28:40 -0500

    remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ericminassian.com",
-  integrations: [tailwind(), prefetch(), sitemap(), partytown()]
+  integrations: [tailwind(), prefetch(), sitemap()],
 });
